67c13c8d20280a485cbf02b94ef3aae78d36bd81
JordanDJ <43160789+JordanDJ@users.noreply.github.com>
2018-09-10 23:22:46 -0400

    Add files via upload

---
+++ SecondPython Jordan Johnson.py
@@ -0,0 +1,12 @@
+# Chapter 2: Input,Processing, and Output Tutorial
+# Sep 5,2018 1:56
+# CTI-110 P2T1 - Sales Prediction
+# Jordan Johnson
+# Get the projected total sales.
+total_sales = float( input('Enter the projected sales: '))
+# Calculate te profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
